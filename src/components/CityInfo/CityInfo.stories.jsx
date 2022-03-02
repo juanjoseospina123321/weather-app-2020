b4b7c3c5797697ada0ad02ee17b5c8a50945dc77
juanjoseospina123321 <juan.jose.ospina@hotmail.com>
2022-03-02 02:06:31 -0500

    Actualizacion storyBook

--- src/components/CityInfo/CityInfo.stories.jsx
+++ src/components/CityInfo/CityInfo.stories.jsx
@@ -4,7 +4,13 @@ import CityInfo from './CityInfo'
 
 export default {
     title: "CityInfo",
-    component: CityInfo
+    component: CityInfo,
+    argTypes:{
+        city:{control:{type:"text"}},
+        country:{control:{type:"text"}}
+    }
 }
 
-export const CityExample = () => (<CityInfo city={"Buenos Aires"} country={"Argentina"}></CityInfo>)
+export const CityExample = (args) => (<CityInfo {...args}></CityInfo>)
+
+CityExample.args={city:"Buenos Aires", country:"Argentina"}
